2ee051de8456cd4a3349295f2140f950a6236aaf
Michael Bauer <mihi@lo-res.org>
2015-06-28 15:48:57 +0200

    bump version to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject archieclj "0.1.0"
+(defproject archieclj "0.1.1-SNAPSHOT"
   :description "An archieml parser for clojure"
   :url "https://github.com/mihi-tr/archieclj"
   :license {:name "Eclipse Public License"
